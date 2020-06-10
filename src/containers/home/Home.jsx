c71540b786af5bac913ee12196c6ae46f5727556
kevin uehara <uehara.kevin@gmail.com>
2020-06-09 23:29:56 -0300

    feat: implements loading and status snackbar

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Form from "../../components/form/form.component";
 import Header from "../../components/header/header.component";
 import DataTable from "../../components/data-table/data-table.component";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+
 import axios from "axios";
 
 import "./Home.css";
@@ -12,6 +15,10 @@ export default class Home extends React.Component {
 
     this.state = {
       phrases: [],
+      loading: true,
+      showDialog: false,
+      success: false,
+      message: ''
     };
   }
 
@@ -20,10 +27,11 @@ export default class Home extends React.Component {
   }
 
   getPhrases() {
+    this.setState({ loading: true });
     axios
       .get(`https://discord-server-kaguya.herokuapp.com/phrases`)
       .then((res) => {
-        this.setState({ phrases: res.data });
+        this.setState({ phrases: res.data, loading: false });
       });
   }
 
@@ -31,7 +39,12 @@ export default class Home extends React.Component {
     axios
       .post(`https://discord-server-kaguya.herokuapp.com/phrases`, data)
       .then((res) => {
-        this.getPhrases();
+        if (res.status === 200) {
+          this.setState({showDialog: true, success: true, message: 'Sucesso ao salvar uhuu :)'})
+          this.getPhrases();
+        } else {
+          this.setState({showDialog: true, success: false, message: 'Erro ao salvar :((  Fala com o kevs'})
+        }
       });
   };
 
@@ -39,26 +52,48 @@ export default class Home extends React.Component {
     axios
       .delete(`https://discord-server-kaguya.herokuapp.com/phrases/${id}`)
       .then((res) => {
-        this.getPhrases();
+        if (res.status === 200) {
+          this.setState({showDialog: true, success: true, message: 'Sucesso ao remover uhuu :)'})
+          this.getPhrases();
+        } else {
+          this.setState({showDialog: true, success: false, message: 'Erro ao remover :(( Fala com o kevs'})
+        }
       });
+  };
+
+  handleCloseDialog = () => {
+    this.setState({ showDialog: false });
   };
 
   render() {
     return (
       <div className="content-app">
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          open={this.state.showDialog}
+          onClose={this.handleCloseDialog}
+          message={this.state.message}
+        />
+
         <div className="header">
           <Header title="Kaguya"></Header>
         </div>
+        <span className="legend">Criado pelo Kevs</span>
+
         <div className="form-container">
           <Form handleSubmit={this.handleSubmit}></Form>
         </div>
 
         <div className="table-container">
-          <DataTable
-            columns={["citação", "autor", "criado em", "remover"]}
-            dataset={this.state.phrases}
-            handleDelete={this.handleDelete}
-          ></DataTable>
+          {this.state.loading ? (
+            <CircularProgress />
+          ) : (
+            <DataTable
+              columns={["citação", "autor", "criado em", "remover"]}
+              dataset={this.state.phrases}
+              handleDelete={this.handleDelete}
+            ></DataTable>
+          )}
         </div>
       </div>
     );
